88cca55f54f7a26cf333cb88cdb24f0669d3930a
Malotaibi <76629831+MBAalotaibi@users.noreply.github.com>
2024-01-19 18:00:05 +0000

    Add files via upload
     Bug1 Algorithm

---
+++ MyLab5Controller.java
@@ -0,0 +1,191 @@
+// MyLab5Controller.java
+/*
+ * MyLab5Controller Class Definition
+ * File: MyLab5Controller.java
+ * Date: 15th Oct 2022
+ * Description: Simple Controller based on the Lab4 controller (2022)
+ * Author: Terry Payne (trp@liv.ac.uk)
+ */
+
+import com.cyberbotics.webots.controller.Supervisor;
+import com.cyberbotics.webots.controller.Camera;
+
+
+import com.cyberbotics.webots.controller.PositionSensor;////Getting postion sensors to track the distance robot traveled using the number of turns
+import java.lang.Math; 
+
+
+public class MyLab5Controller {
+
+  public static enum GlobalState {//Global states in robot for bug 1 
+    GO_TO_GOAL,
+    WALL_FOLLOW};
+
+  public static void main(String[] args) {
+
+    Supervisor robot = new Supervisor();
+    int timeStep = (int) Math.round(robot.getBasicTimeStep());
+    
+    /////////////////////////////////////////////////////////////////
+    ////////////////////////Setting  target and start coordinates
+    PioneerNav2.MoveState state; // current naviagation state
+    double[] start = {-4.5,3,0};
+    double[] target = {2.74859 ,-3.25946 };
+    GlobalState global_state = GlobalState.GO_TO_GOAL;///Setting starting state to go to goal
+    state = PioneerNav2.MoveState.ARC;
+    double  start_time = robot.getTime();/////getting start time
+    
+    /////////////////////////////////////////////////////////////////
+    Pose robot_pose = new Pose(start[0], start[1], start[2]);///Giving starting coordinates to pose class
+    //PioneerNav2 nav = new PioneerNav2(robot, robot_pose);
+    //PioneerProxSensors1 prox_sensors = new PioneerProxSensors1(robot, "sensor_display", robot_pose);
+    Camera camera = robot.getCamera("camera");
+    
+    if (camera != null)
+      camera.enable(timeStep);
+    //////////////////////////////////////////////////initailzing position sensors
+    PositionSensor left_position_sensor = robot.getPositionSensor("left wheel sensor");
+    left_position_sensor.enable(timeStep);
+    PositionSensor right_position_sensor = robot.getPositionSensor("right wheel sensor");
+    right_position_sensor.enable(timeStep);
+    ///////////////////////////////////////////////////////////////////////
+    PioneerProxSensors1 prox_sensors = new PioneerProxSensors1(robot, "sensor_display", robot_pose);
+    PioneerNav2 nav = new PioneerNav2(robot, robot_pose, prox_sensors);
+
+    double time_elapsed = 0;
+    double target_time = 0;
+    double robot_velocity = 0.3;
+    
+    // define schedule
+    //PioneerNav2.MoveState[] schedule = { PioneerNav2.MoveState.FOLLOW_WALL };
+    ////int schedule_index = -1; // we increment before selecting the current action
+    
+    
+    double min_distance = Math.sqrt(Math.pow((target[0] - start[0]), 2) + Math.pow(target[1] - start[1], 2)); ///Variabble to save minimum distance
+    //double[] coords_min = {start[0],start[1]};//Coordintes at minimum distance found
+    double distance_to_travel = min_distance;//Distance variable given to the move ford state
+    double[] wall_follow_start_cord = {0,0}; //Location of wall follow starting
+    int no_update = 0;
+    boolean update_min = true; 
+    double theta = 0;
+    double cord_at_min_distance[] = {0,0};///Coordinates at the minimum distance to target
+    
+    
+    
+    
+   
+    while (robot.step(timeStep) != -1) {
+      // Testing out the front proximity sensors
+     
+      robot_pose = nav.get_real_pose();
+      prox_sensors.set_pose(robot_pose);
+      prox_sensors.paint();  // Render sensor Display
+      System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+      System.out.print("Global state :");
+      System.out.println(global_state);
+      System.out.print("Navigation state :");
+      System.out.println(state);
+      System.out.print("Distance traveled :");
+      System.out.print(0.0957*(right_position_sensor.getValue() + left_position_sensor.getValue())/2);
+      System.out.println(" meters");
+      if (Math.sqrt(Math.pow((target[0] - robot_pose.getX()), 2) + Math.pow(target[1] - robot_pose.getY(), 2)) < 0.05)
+      { ///If close to the target then termicate the code
+           time_elapsed = target_time+1;
+           nav.stop();
+           state = PioneerNav2.MoveState.STOP; 
+           System.out.print("Time taken :");
+           System.out.print(robot.getTime() - start_time);
+           System.out.println(" seconds");
+           System.out.print("Distance traveled :");
+           System.out.print(0.0957*(right_position_sensor.getValue() + left_position_sensor.getValue())/2);
+           System.out.println(" meters");
+           break;
+             }
+      
+      if (global_state == GlobalState.GO_TO_GOAL)//state where the robot turns and moves towards the goal
+      {
+        double y = target[1] - robot_pose.getY();
+        double x = target[0] - robot_pose.getX();
+        theta = Math.atan2(y, x) - robot_pose.getTheta();//Getting the heading difference with the target
+        if (Math.abs(theta)>0.02)//IF the heading heading differentce and taget differntce is bigger then turning the robot
+        {
+          state = PioneerNav2.MoveState.ARC ;
+        }else//Else moving towards the target
+        {
+          state = PioneerNav2.MoveState.FORWARD ;
+          if (prox_sensors.get_value(4)  <0.2)//IF there is an obstacle ahead getting ready to do wallfollowing
+          {
+            time_elapsed = target_time+1; //Removing form the MoveState.FORWARD by completeing its time
+            global_state = GlobalState.WALL_FOLLOW;//Changing global state to wall following
+            
+            //Setting wallfollow start cords
+            wall_follow_start_cord[0] = robot_pose.getX();
+            wall_follow_start_cord[1] = robot_pose.getY();
+            
+            update_min = true; //Update the minimum distance to target
+            no_update = 0 ;//Varibale to count the times that there were no update to wall following start location
+          }
+        }
+      } 
+       if (global_state == GlobalState.WALL_FOLLOW)
+      {
+        state = PioneerNav2.MoveState.FOLLOW_WALL;//Setting navigation state to follow wall 
+         double distance = Math.sqrt(Math.pow((target[0] - robot_pose.getX()), 2) + Math.pow(target[1] - robot_pose.getY(), 2));//getting distance to target
+         no_update +=1 ; //countitng the times where the robot hasnt been in the wall followa start area
+         if ((Math.sqrt(Math.pow((wall_follow_start_cord[0] - robot_pose.getX()), 2) + Math.pow(wall_follow_start_cord[1] - robot_pose.getY(), 2)) <0.25) && (no_update >100) )
+         {
+               ///IF the no update varibale if greater than 100 and the robot is in the wall follow start area then robo has completed a round 
+               update_min = false; ///Stapping updating the minimum distance to target
+        }
+        else //else the robot is still near the start area
+        if (Math.sqrt(Math.pow((wall_follow_start_cord[0] - robot_pose.getX()), 2) + Math.pow(wall_follow_start_cord[1] - robot_pose.getY(), 2)) <0.25 )
+         {
+                no_update = 0; //making the update variabke 0 
+        }
+        
+         if ((min_distance> distance) && update_min )///updatin the distance minmumum 
+         {
+               min_distance  = distance ; 
+               cord_at_min_distance[0] = robot_pose.getX();
+               cord_at_min_distance[1] = robot_pose.getY();
+               }
+          ///if the robot is not updating the minimum  anymore and meets the minimum then change state to motion to goal     
+          if ((Math.sqrt(Math.pow((cord_at_min_distance[0] - robot_pose.getX()), 2) + Math.pow(cord_at_min_distance[1] - robot_pose.getY(), 2)) <0.3) && !update_min  )
+         {
+               global_state = GlobalState.GO_TO_GOAL;///Switching the state to go to goal since the robot is ready to start doing wall following again
+        }
+      }
+      
+      if (time_elapsed > target_time) {
+        time_elapsed = 0;
+            
+        // select next action in schedule if not stopped
+        //schedule_index = (schedule_index + 1) % schedule.length;
+       
+        if (state == PioneerNav2.MoveState.FOLLOW_WALL)
+        {
+        target_time = 0; // always refresh!
+        nav.follow_wall(robot_velocity, 0.25, true);
+        }else
+        if (state == PioneerNav2.MoveState.FORWARD) {
+          target_time = nav.forward(distance_to_travel, robot_velocity);
+        } else 
+        if (state == PioneerNav2.MoveState.ARC) {
+          target_time = nav.arc(theta, 0.0, robot_velocity);
+        } else
+        if (state == PioneerNav2.MoveState.STOP) {
+          nav.stop();
+          target_time = 60 * 1000; // This doesn't really stop, but pauses for 1 minute
+          
+        }
+        
+      } else
+        time_elapsed += timeStep;    // Increment by the time state
+      
+    };
+    // Enter here exit cleanup code.
+    nav.stop();
+    robot.step(timeStep);
+  }
+  
+}
---
+++ PioneerNav2.java
@@ -0,0 +1,217 @@
+// PioneerNav2.java
+/*
+ * PioneerNavigation Class Definition
+ * File: PioneerNav2.java
+ * Date: 18th Oct 2022
+ * Description: Simple Navigation Class support (2022)
+ * Author: Terry Payne (trp@liv.ac.uk)
+ */
+ 
+import com.cyberbotics.webots.controller.Motor;
+import com.cyberbotics.webots.controller.Supervisor;
+import com.cyberbotics.webots.controller.Node;
+import java.lang.Math;
+
+
+public class PioneerNav2 {
+
+  public static enum MoveState {
+    STOP,
+    FORWARD,
+    ARC,
+    FOLLOW_WALL };
+
+  private Supervisor robot;   // reference to the robot
+  private Node robot_node;    // reference to the robot node
+  private Pose robot_pose;    // the robots percieved pose
+  private Motor left_motor;
+  private Motor right_motor;
+  private double max_vel;
+  private double prev_error;
+  private double total_error;
+  private MoveState state;
+  private PioneerProxSensors1 prox_sensors;
+
+  private final double WHEEL_RADIUS = 0.0957; // in meters - found using CONFIGURE 
+  private final double AXEL_LENGTH = 0.323;   // in meters - found using CONFIGURE
+
+
+  // ==================================================================================
+  // Constructor
+  // ==================================================================================
+  public PioneerNav2(Supervisor robot, Pose init_pose,PioneerProxSensors1 ps) {
+    this.robot = robot;    
+    this.robot_node = this.robot.getSelf();   // reference to the robot node
+    this.state = MoveState.STOP;
+
+    // enable motors
+    this.left_motor = robot.getMotor("left wheel");
+    this.right_motor = robot.getMotor("right wheel");
+    this.left_motor.setPosition(Double.POSITIVE_INFINITY);
+    this.right_motor.setPosition(Double.POSITIVE_INFINITY);
+    
+    int timeStep = (int) Math.round(robot.getBasicTimeStep());
+    
+    this.prox_sensors = ps;
+    
+    // set up pose
+    this.robot_pose = new Pose(init_pose.getX(), init_pose.getY(), init_pose.getTheta());
+
+    // Initialise motor velocity
+    this.left_motor.setVelocity(0.0);
+    this.right_motor.setVelocity(0.0);  
+    
+    this.max_vel = this.left_motor.getMaxVelocity() - 0.1; 
+    this.prev_error = 0;
+    this.total_error = 0;
+      // reference to proximity sen
+  } 
+  
+
+  // The following method only works if in supervisor mode
+  public Pose get_real_pose() {
+    if (this.robot_node == null)
+      return new Pose(0,0,0);
+      
+    double[] realPos = robot_node.getPosition();
+    double[] rot = this.robot_node.getOrientation(); // 3x3 Rotation matrix as vector of length 9
+    double theta1 = Math.atan2(-rot[0], rot[3]);
+    double halfPi = Math.PI/2;
+    double theta2 = theta1 + halfPi;
+    if (theta1 > halfPi)
+        theta2 = -(3*halfPi)+theta1;
+    
+    return new Pose(realPos[0], realPos[1], theta2);
+  }
+
+  public int forward(double target_dist, double robot_linearvelocity) {
+    double wheel_av = (robot_linearvelocity/this.WHEEL_RADIUS);
+    double target_time = target_dist/robot_linearvelocity;
+    
+    this.left_motor.setVelocity(wheel_av);
+    this.right_motor.setVelocity(wheel_av);
+    this.state = MoveState.FORWARD;
+        
+    // return target_time as millisecs          
+    return (int) (1000.0*target_time);
+  }
+
+  public int arc(double icr_angle, double icr_r, double icr_omega) {
+    double target_time = Math.abs(icr_angle) / icr_omega;
+    double vl = 0;
+    double vr = 0;
+    // Calculate each wheel velocity around ICR
+    if(icr_angle> 0)
+    {
+    vl = icr_omega * (icr_r - (this.AXEL_LENGTH / 2));
+   vr = icr_omega * (icr_r + (this.AXEL_LENGTH / 2));
+     }
+     else{
+      vl = icr_omega * (icr_r + (this.AXEL_LENGTH / 2));
+     vr = icr_omega * (icr_r - (this.AXEL_LENGTH / 2));
+     }   
+    double leftwheel_av = (vl/this.WHEEL_RADIUS);
+    double rightwheel_av = (vr/this.WHEEL_RADIUS);
+        
+    this.left_motor.setVelocity(leftwheel_av);
+    this.right_motor.setVelocity(rightwheel_av);
+    this.state = MoveState.ARC;
+
+    // return target_time as millisecs          
+    return (int) (1000.0*target_time);
+  }  
+  public void stop() {
+    
+    this.left_motor.setVelocity(0.0);
+    this.right_motor.setVelocity(0.0);
+    this.state = MoveState.STOP;
+  }
+  
+  public MoveState getState() {
+    return this.state;
+  }
+  
+  public void set_velocity(double base, double control) {
+  
+  // base gives the velocity of the wheels in m/s
+  // control is an adjustment on the main velocity
+  double base_av = (base/this.WHEEL_RADIUS);
+ 
+  double lv = base_av;
+  double rv = base_av;
+  if (control != 0) {
+  double control_av = (control/this.WHEEL_RADIUS);
+  // Check if we exceed max velocity and compensate
+  double correction = 1;
+  lv = base_av - control_av;
+  rv = base_av + control_av;
+  if (lv > this.max_vel) {
+  correction = this.max_vel / lv;
+  lv = lv * correction;
+  rv = rv * correction;
+  }
+  if (rv > this.max_vel) {
+  correction = this.max_vel / rv;
+  lv = lv * correction;
+  rv = rv * correction;
+  }
+  }
+  
+  
+  this.left_motor.setVelocity(lv);
+  this.right_motor.setVelocity(rv);
+  }
+  
+  private double pid(double error) {
+  double kp = 0.8; // proportional weight (may need tuni 0.6
+  double kd = 3.0; // differential weight (may need tuni
+  double ki = 0.0; // integral weight (may need tuning)
+  
+  double prop = error;
+  double diff = error - this.prev_error;
+  this.total_error += error;
+  double control = (kp * prop) + (ki * this.total_error) + (kd * diff);
+  this.prev_error = error;
+  return control;
+  }
+  
+  public void follow_wall(double robot_linearvelocity, double set_point, boolean right) {
+  
+  int direction_coeff = 1;
+  double error;
+  double control;
+  double wall_dist;
+  if (right) direction_coeff = -1;  // invert the values for the control
+  
+  if (Math.min(this.prox_sensors.get_value(1),
+  Math.min(this.prox_sensors.get_value(2),
+  Math.min(this.prox_sensors.get_value(3),
+  Math.min(this.prox_sensors.get_value(4),
+  Math.min(this.prox_sensors.get_value(5),
+  this.prox_sensors.get_value(6)))))) < set_point){
+  
+  this.set_velocity(robot_linearvelocity, -0.8*direction_coeff);
+ 
+  }
+  
+  else{
+ 
+  if (!right) wall_dist = Math.min(this.prox_sensors.get_value(1),this.prox_sensors.get_value(0));
+  else{
+  
+   wall_dist = Math.min(this.prox_sensors.get_value(7),this.prox_sensors.get_value(8));
+   }
+  if (wall_dist < this.prox_sensors.get_maxRange()) {
+  error = wall_dist - set_point;
+  control = this.pid(error);
+  this.set_velocity(robot_linearvelocity, control*direction_coeff);
+  }
+  else{
+  this.set_velocity(robot_linearvelocity, 0.15*direction_coeff);
+  }
+  }
+  this.state = MoveState.FOLLOW_WALL;
+  }
+  
+  
+}    
